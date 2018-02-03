223c259e1f0982a2fa95d0ecc2512765a27b18ef
Margaret Rumble <megrumble@gmail.com>
2018-02-03 16:02:47 -0500

    saved/notes

--- public/app.js
+++ public/app.js
@@ -24,10 +24,77 @@
 //     $("#articles").append(container);
 //   }
 // });
+//Handle Scrape button
+$("#scrape").on("click", function() {
+  $.ajax({
+      method: "GET",
+      url: "/scrape",
+  }).done(function(data) {
+      console.log(data)
+  })
+});
+
+//Handle Save Article button
+$(".save").on("click", function() {
+  var thisId = $(this).attr("data-id");
+  $.ajax({
+      method: "POST",
+      url: "/articles/save/" + thisId
+  }).done(function(data) {
+      window.location = "/"
+  })
+});
 
 
+//Handle Delete Article button
+$(".delete").on("click", function() {
+  var thisId = $(this).attr("data-id");
+  $.ajax({
+      method: "POST",
+      url: "/articles/delete/" + thisId
+  }).done(function(data) {
+      window.location = "/saved"
+  })
+});
+
+//Handle Save Note button
+$(".saveNote").on("click", function() {
+  var thisId = $(this).attr("data-id");
+  if (!$("#noteText" + thisId).val()) {
+      alert("please enter a note to save")
+  }else {
+    $.ajax({
+          method: "POST",
+          url: "/notes/save/" + thisId,
+          data: {
+            text: $("#noteText" + thisId).val()
+          }
+        }).done(function(data) {
+            // Log the response
+            console.log(data);
+            // Empty the notes section
+            $("#noteText" + thisId).val("");
+            $(".modalNote").modal("hide");
+            window.location = "/saved"
+        });
+  }
+});
+
+//Handle Delete Note button
+$(".deleteNote").on("click", function() {
+  var noteId = $(this).attr("data-note-id");
+  var articleId = $(this).attr("data-article-id");
+  $.ajax({
+      method: "DELETE",
+      url: "/notes/delete/" + noteId + "/" + articleId
+  }).done(function(data) {
+      console.log(data)
+      $(".modalNote").modal("hide");
+      window.location = "/saved"
+  })
+});
 // Whenever someone clicks a p tag
-$("#articles").on("click", "p", function () {
+$("#articles").on("click", "h4", function () {
   console.log("hi");
   // Empty the notes from the note section
   $("#notes").empty();
